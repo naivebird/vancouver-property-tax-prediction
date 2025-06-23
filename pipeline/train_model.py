1f5a72afa62a3f4ecb1fdf8c062c16f3436a14b8
Duc Ho <hoduc1202@gmail.com>
2025-06-23 22:48:23 +0000

    Pending changes exported from your codespace

--- pipeline/train_model.py
+++ pipeline/train_model.py
@@ -1,19 +1,21 @@
 from pathlib import Path
-import pickle
+from typing import List
 import pandas as pd
 import numpy as np
 import scipy
 import sklearn
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import root_mean_squared_error, r2_score
 import mlflow
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
 from mlflow.tracking import MlflowClient
+from sklearn.pipeline import make_pipeline
+
 
 from prefect import flow, task
 
-TRACKING_URI = "http://ec2-54-244-11-127.us-west-2.compute.amazonaws.com:5000/"
+TRACKING_URI = "http://ec2-44-243-35-172.us-west-2.compute.amazonaws.com:5000/"
 
 
 @task(retries=3, retry_delay_seconds=2)
@@ -50,49 +52,53 @@ def add_features(
         'property_postal_code'
         ]
 
-    dv = DictVectorizer()
-
     df_train.dropna(inplace=True)
-    train_dicts = df_train[features].to_dict(orient="records")
-    X_train = dv.fit_transform(train_dicts)
-
     df_val.dropna(inplace=True)
+
+
+    for col in ['year_built', 'big_improvement_year', 'tax_assessment_year']:
+        df_train[col] = df_train[col].astype(int)
+        df_val[col] = df_val[col].astype(int)
+
+    train_dicts = df_train[features].to_dict(orient="records")
+
     val_dicts = df_val[features].to_dict(orient="records")
-    X_val = dv.transform(val_dicts)
 
     y_train = df_train["tax_levy"].values
     y_val = df_val["tax_levy"].values
-    return X_train, X_val, y_train, y_val, dv
+    return train_dicts, val_dicts, y_train, y_val
 
 
 @task(log_prints=True)
 def train_model(
-    X_train: scipy.sparse._csr.csr_matrix,
-    X_val: scipy.sparse._csr.csr_matrix,
+    train_dicts: List[dict],
+    val_dicts: List[dict],
     y_train: np.ndarray,
-    y_val: np.ndarray,
-    dv: sklearn.feature_extraction.DictVectorizer,
+    y_val: np.ndarray
 ) -> None:
     """Train a model and register it to the model registry"""
 
     with mlflow.start_run() as run:
 
-        lr = LinearRegression()
-        lr.fit(X_train, y_train)
+        pipeline = make_pipeline(
+        DictVectorizer(),
+        LinearRegression()
+    )
+        pipeline.fit(train_dicts, y_train)
 
-        y_pred = lr.predict(X_val)
+        y_pred = pipeline.predict(val_dicts)
+
         rmse = root_mean_squared_error(y_val, y_pred)
+        r2 = r2_score(y_val, y_pred)
+
         mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
 
-        Path("models").mkdir(exist_ok=True)
-        with open("models/preprocessor.b", "wb") as f_out:
-            pickle.dump(dv, f_out)
-        mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
-
-        mlflow.sklearn.log_model(lr, artifact_path="model")
+        mlflow.sklearn.log_model(pipeline, artifact_path="model")
 
         run_id = run.info.run_id
         model_uri = f"runs:/{run_id}/model"
+        print("register model")
 
         registered_model = mlflow.register_model(
             model_uri=model_uri,
@@ -108,7 +114,7 @@ def train_model(
 
         try:
             prod_model = mlflow.pyfunc.load_model(f"models:/{registered_model.name}/Production")
-            prod_y_pred = prod_model.predict(X_val)
+            prod_y_pred = prod_model.predict(val_dicts)
             prod_rmse = root_mean_squared_error(y_val, prod_y_pred)
 
             if rmse > prod_rmse:
@@ -147,10 +153,10 @@ def main_flow(
     df_val = read_data(val_path)
 
     # Transform
-    X_train, X_val, y_train, y_val, dv = add_features(df_train, df_val)
+    train_dicts, val_dicts, y_train, y_val = add_features(df_train, df_val)
 
     # Train
-    train_model(X_train, X_val, y_train, y_val, dv)
+    train_model(train_dicts, val_dicts, y_train, y_val)
 
 
 if __name__ == "__main__":
